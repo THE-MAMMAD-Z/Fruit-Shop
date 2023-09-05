aae919caca2adafb2b81ae3839bd8f66a24fdff8
mohammad <mohammadz270@yahoo.com>
2023-09-05 19:40:09 +0430

    github

--- core/settings.py
+++ core/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'MIVE',
         'USER': 'postgres',
-        'PASSWORD': 'sezar1381',
+        'PASSWORD': '1111111',
         'HOST': 'localhost',
         'PORT': '5432',
     }
